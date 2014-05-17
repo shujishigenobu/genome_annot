fff5f6aa090984d9b3319757b159457b0f3c5996
Shuji Shigenobu <sshigenobu@gmail.com>
2014-05-17 13:30:12 +0900

    #1 fixed: exonerate_to_JBGFF3.rb

--- converters/exonerate_to_JBGFF3.rb
+++ converters/exonerate_to_JBGFF3.rb
@@ -10,14 +10,15 @@ File.open(exonerate_out).each do |l|
     target = m.post_match.split[0]
   elsif /^#{target}/.match(l) &&
       (/\texonerate:est2genome\t/.match(l) || /\texonerate:protein2genome:local\t/.match(l)) &&
-      (/\tcds\t/.match(l) || /\tgene\t/.match(l))
+      (/\tcds\t/.match(l) || /\texon\t/.match(l) || /\tgene\t/.match(l))
+#    puts l
     a = l.chomp.split(/\t/)
     b = Array.new(9)
     a.each_with_index{|x, i| b[i] = x}
     if b[2] == "gene"
       b[-1] = "ID=#{name}"
       b[2] = "match"
-    elsif b[2] == "cds"
+    elsif (b[2] == "cds" || b[2] == "exon")
       b[-1] = "Parent=#{name}"
       b[2] = "match_part"
     else
@@ -27,4 +28,3 @@ File.open(exonerate_out).each do |l|
   end
 
 end
-# puts [name, target]
