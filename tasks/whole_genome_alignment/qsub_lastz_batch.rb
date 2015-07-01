3653a25006828eb277df06ce21d6e338b63710e0
Shuji Shigenobu <sshigenobu@gmail.com>
2015-07-01 12:21:28 +0900

    qsub_lastz_batch.rb: new parameters in lastz exec

--- tasks/whole_genome_alignment/qsub_lastz_batch.rb
+++ tasks/whole_genome_alignment/qsub_lastz_batch.rb
@@ -10,7 +10,11 @@ qcontigs = File.open(qlistf).readlines.map{|x| x.strip}
 rcontigs = File.open(rlistf).readlines.map{|x| x.strip}
  
 template = "lastz #{rseqdir}/REF.nib #{qseqdir}/QUERY.nib H=2000 Y=3400 L=6000 K=2200 --format=lav | lavToPsl stdin stdout "
- 
+
+## Template where lastz parameters are optimized for a pair of very similar genomes such as Human-Chimp
+# template = "lastz #{rseqdir}/REF.nib #{qseqdir}/QUERY.nib H=0 Y=15000 L=3000 K=4500 E=150 O=600 T=2 --format=lav | lavToPsl stdin stdout " 
+
+
 p qcontigs.size
 p rcontigs.size
  
