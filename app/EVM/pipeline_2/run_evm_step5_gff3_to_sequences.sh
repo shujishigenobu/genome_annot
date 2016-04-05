92d3d0613fa7ccea52118e40ac289c03ef440ee9
Shuji Shigenobu <sshigenobu@gmail.com>
2016-04-05 11:57:29 +0900

    small improvement

--- app/EVM/pipeline_2/run_evm_step5_gff3_to_sequences.sh
+++ app/EVM/pipeline_2/run_evm_step5_gff3_to_sequences.sh
@@ -1,7 +1,7 @@
-GENOME=genome.fa
-GFF=Dnox.EVM2.gff3
-OUTBASE=Dnox.EVM2
-EVMDIR=~/GitHub/EVidenceModeler
+GENOME=../Heike2.2.final.assembly.fasta
+GFF=Heike.EVM2.3.gff3
+OUTBASE=Heike.EVM2.3
+EVMDIR=/home/shige/Projects/Aphid/Analysis/150804-Oulm_genome_annot/150811-EVM/EVidenceModeler
 
 $EVMDIR/EvmUtils/gff3_file_to_proteins.pl \
   $GFF    \
@@ -15,3 +15,11 @@ $EVMDIR/EvmUtils/gff3_file_to_proteins.pl \
   $GENOME \
   CDS     \
   > $OUTBASE.cds
+
+## clean fasta definition lines
+
+mv $OUTBASE.pep $OUTBASE.pep.orig
+mv $OUTBASE.cds $OUTBASE.cds.orig
+
+ruby clean_evm_pep_deflin.rb $OUTBASE.pep.orig > $OUTBASE.pep
+ruby clean_evm_pep_deflin.rb $OUTBASE.cds.orig > $OUTBASE.cds
